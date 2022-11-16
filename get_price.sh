ababe067397a7145299092ec18353f5742003bf8
dannpr <dannba0@gmail.com>
2022-11-16 18:56:18 +0000

    pull

--- get_price.sh
+++ get_price.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl https://minswap.org/ > minprice.txt
+curl https://minswap.org/ > minpage.txt
 echo "Currently running ..."
 echo "This is the current  price of MIN" > minprice.txt
 echo " " >> minprice.txt
 
-cat minprice.txt | grep -oPz '(?<=div)(.)+?(?=div)' 
+cat minpage.txt | grep -oPz '(?<=div)(.)+?(?=div)' 
 
