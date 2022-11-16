a7f8bba27709a1b8f41c6257e03f0cf87fb8ca4f
dannpr <dannba0@gmail.com>
2022-11-16 21:50:49 +0000

    jhbv

--- get_price.sh
+++ get_price.sh
@@ -6,7 +6,11 @@ echo "This is the current  price of MIN" > minprice.txt
 echo " " >> minprice.txt
 
 #Get price data
-token_name=$(cat minpage.txt | grep -Poz '(?<=class="jet-listing-dynamic-field__content">).*(?<=MIN)') | head -1
+token_name=$(cat minpage.txt | grep -Poz '(?<=class="jet-listing-dynamic-field__content">).*(?<=MIN)' | head -1)
 echo "Name: $token_name">>minprice.txt
+
+token_price=$(cat minpage.txt | grep -Poz '(?<=class="jet-listing-dynamic-field__content">).*(?<=вв‚і)')
+echo "Price: $token_price">>minprice.txt
+
 
 cat minprice.txt
