ba9d7730e1a5ae25568337a4b5d8d96ddafda435
dannpr <dannba0@gmail.com>
2022-11-16 20:41:47 +0000

    ll

--- get_price.sh
+++ get_price.sh
@@ -5,5 +5,6 @@ echo "Currently running ..."
 echo "This is the current  price of MIN" > minprice.txt
 echo " " >> minprice.txt
 
-cat minpage.txt | grep -oPz '(?<=div)(.)+?(?=div)' 
-
+#Get price data
+token_name=$(echo  minpage.txt | grep -Po '(?<=class="jet-listing-dynamic-field__content">).*(Â<=N)')
+echo $token_name
