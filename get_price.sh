395ae7f3d5ea2037ee1046cb9cdb4953ad963fa6
dannpr <dannba0@gmail.com>
2022-11-16 15:59:40 +0000

    new

--- get_price.sh
+++ get_price.sh
@@ -5,4 +5,4 @@ echo "Currently running ..."
 echo "This is the current  price of MIN" > minprice.txt
 echo " " >> minprice.txt
 
-cat minprice.txt | grep -oPz '(?<=div)(.)+?(?=div)'  
+cat minpage.txt | grep -oPz '(?<=<p)(.)+?(?=p)'
