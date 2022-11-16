2bee62845de158a5d21567a33e0d5d6e91932a6b
dannpr <dannba0@gmail.com>
2022-11-16 16:09:19 +0000

    new

--- get_price.sh
+++ get_price.sh
@@ -5,4 +5,13 @@ echo "Currently running ..."
 echo "This is the current  price of MIN" > minprice.txt
 echo " " >> minprice.txt
 
-cat minpage.txt | grep -oPz '(?<=<p)(.)+?(?=p)'
+cat minpage.txt | grep -oPz '(?<=<p)(.)+?(?=p)' | while read -r line ; do 
+pat1='class="jet-listing-dynamic-field__content"'
+
+if [[ $line =~ $pat1 ]];
+then
+name=$(echo $line | grep -Po '(?<=if [[ $line =~ $pat1 ]];
+then
+price=$(echo $line | grep -Po '(?<=class="jet-listing-dynamic-field__content" data-sort=.).*(?=.>)')
+echo "Price: $price" >>minprice.txt
+fi 
